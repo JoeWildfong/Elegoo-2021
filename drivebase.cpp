02c7e17d8eba4db329c8bc097c84b0e98cd75dd5
JoeWildfong <wildfong.joe@gmail.com>
2021-04-14 16:10:07 -0400

    Drivebase: clamp input to TankDrive

--- drivebase.cpp
+++ drivebase.cpp
@@ -1,17 +1,14 @@
 #include "include/drivebase.hpp"
 
-void Drivebase::init() {
-    pinMode(Drivebase::LEFT_SPEED_PIN, OUTPUT);
-    pinMode(Drivebase::RIGHT_SPEED_PIN, OUTPUT);
-    pinMode(Drivebase::LEFT_FORWARD_PIN, OUTPUT);
-    pinMode(Drivebase::LEFT_BACKWARD_PIN, OUTPUT);
-    pinMode(Drivebase::RIGHT_FORWARD_PIN, OUTPUT);
-    pinMode(Drivebase::RIGHT_BACKWARD_PIN, OUTPUT);
-    digitalWrite(LEFT_SPEED_PIN, HIGH);
-    digitalWrite(RIGHT_SPEED_PIN, HIGH);
+static double copySign(double dest, double source) {
+    return source >= 0 ? dest : -dest;
 }
 
-void Drivebase::TankDrive(double left, double right) {
+static double clamp(double in, double min, double max) {
+    return in < min ? min : in > max ? max : in;
+}
+
+static void setPower(double left, double right) {
     analogWrite(Drivebase::LEFT_SPEED_PIN, round(abs(left) * 1023));
     analogWrite(Drivebase::RIGHT_SPEED_PIN, round(abs(right) * 1023));
 
@@ -38,12 +35,21 @@ void Drivebase::TankDrive(double left, double right) {
     }
 }
 
-static double copySign(double dest, double source) {
-    return source >= 0? dest: -dest;
+void Drivebase::init() {
+    pinMode(Drivebase::LEFT_SPEED_PIN, OUTPUT);
+    pinMode(Drivebase::RIGHT_SPEED_PIN, OUTPUT);
+    pinMode(Drivebase::LEFT_FORWARD_PIN, OUTPUT);
+    pinMode(Drivebase::LEFT_BACKWARD_PIN, OUTPUT);
+    pinMode(Drivebase::RIGHT_FORWARD_PIN, OUTPUT);
+    pinMode(Drivebase::RIGHT_BACKWARD_PIN, OUTPUT);
+    digitalWrite(LEFT_SPEED_PIN, HIGH);
+    digitalWrite(RIGHT_SPEED_PIN, HIGH);
 }
 
-static double clamp(double in, double min, double max) {
-    return in < min? min: in > max? max : in;
+void Drivebase::TankDrive(double left, double right) {
+    left = clamp(left, -1, 1);
+    right = clamp(right, -1, 1);
+    setPower(left, right);
 }
 
 void Drivebase::ArcadeDrive(double y, double x) {
@@ -52,30 +58,28 @@ void Drivebase::ArcadeDrive(double y, double x) {
     double maxInput = copySign(max(abs(y), abs(x)), y);
 
     if (y >= 0.0) {
-      // First quadrant, else second quadrant
-      if (x >= 0.0) {
-        left = maxInput;
-        right = y - x;
-      } else {
-        left = y + x;
-        right = maxInput;
-      }
+        // First quadrant, else second quadrant
+        if (x >= 0.0) {
+            left = maxInput;
+            right = y - x;
+        } else {
+            left = y + x;
+            right = maxInput;
+        }
     } else {
-      // Third quadrant, else fourth quadrant
-      if (x >= 0.0) {
-        left = y + x;
-        right = maxInput;
-      } else {
-        left = maxInput;
-        right = y - x;
-      }
+        // Third quadrant, else fourth quadrant
+        if (x >= 0.0) {
+            left = y + x;
+            right = maxInput;
+        } else {
+            left = maxInput;
+            right = y - x;
+        }
     }
 
-    left = clamp(left, -1.0, 1.0);
-    right = clamp(right, -1.0, 1.0);
-    TankDrive(left, right);
+    left = clamp(left, -1, 1);
+    right = clamp(right, -1, 1);
+    setPower(left, right);
 }
 
-void Drivebase::stop() {
-    Drivebase::TankDrive(0, 0);
-}
+void Drivebase::stop() { Drivebase::TankDrive(0, 0); }
