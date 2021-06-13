3699509cb869eee1627431f810b2f24ad5e7300d
JoeWildfong <wildfong.joe@gmail.com>
2021-06-13 15:49:00 -0400

    Grabber: use constants for min and max angles

--- grabber.cpp
+++ grabber.cpp
@@ -3,45 +3,56 @@
 
 #include <Servo.h>
 
-static constexpr uint8_t grabberServoPin = 3;
+static constexpr uint8_t GRABBER_SERVO_PIN = 3;
 
-static uint8_t servoDeadzone = 0;
-static uint8_t servoAngle = 0;
+static constexpr uint8_t MIN_ANGLE = 0;
+static constexpr uint8_t MAX_ANGLE = 180;
 
 static Servo grabberServo;
 
-void Grabber::init() {
-    grabberServo.attach(grabberServoPin);
-    grabberServo.write(servoAngle);
+static uint8_t grabberServoDeadzone = 0;
+static uint8_t grabberServoAngle = MIN_ANGLE;
+
+static inline uint8_t grabberClamp8(uint8_t angle) {
+    return Util::clamp_8(angle, MIN_ANGLE, MAX_ANGLE);
 }
 
-void Grabber::setDeadzone(uint8_t deadzone) { servoDeadzone = deadzone; }
+static inline int16_t grabberClamp16(int16_t angle) {
+    return Util::clamp_16(angle, MIN_ANGLE, MAX_ANGLE);
+}
+
+void Grabber::init() {
+    grabberServo.attach(GRABBER_SERVO_PIN);
+    grabberServo.write(grabberServoAngle);
+}
+
+void Grabber::setDeadzone(uint8_t deadzone) { grabberServoDeadzone = deadzone; }
 
 void Grabber::setAngle(uint8_t degrees) {
-    servoAngle = Util::clamp_8(degrees, 0, 180);
-    grabberServo.write(servoAngle);
+    grabberServoAngle = grabberClamp8(degrees);
+    grabberServo.write(grabberServoAngle);
 }
 
 void Grabber::changeAngle(int16_t degrees) {
-    if (degrees < servoDeadzone) {
+    if (abs(degrees) < grabberServoDeadzone) {
         return;
     }
-    servoAngle = Util::clamp_16(servoAngle + degrees, 0, 180);
-    grabberServo.write(servoAngle);
+    grabberServoAngle = grabberClamp16(grabberServoAngle + degrees);
+    grabberServo.write(grabberServoAngle);
 }
 
 void Grabber::moveUp(uint8_t degrees) {
-    if (degrees < servoDeadzone) {
+    if (degrees < grabberServoDeadzone) {
         return;
     }
-    servoAngle = Util::clamp_16(servoAngle + degrees, 0, 180);
-    grabberServo.write(servoAngle);
+    grabberServoAngle = grabberClamp16(grabberServoAngle + degrees);
+    grabberServo.write(grabberServoAngle);
 }
 
 void Grabber::moveDown(uint8_t degrees) {
-    if (degrees < servoDeadzone) {
+    if (degrees < grabberServoDeadzone) {
         return;
     }
-    servoAngle = Util::clamp_16(servoAngle - degrees, 0, 180);
-    grabberServo.write(servoAngle);
+    grabberServoAngle = grabberClamp16(grabberServoAngle - degrees);
+    grabberServo.write(grabberServoAngle);
 }
